fbb2071b069062af5b2abb50ac6412fa3c7466b0
Glitch (breezy-spot-color) <none>
2021-11-18 00:34:33 +0000

    👧🏦 Checkpoint ./js/script.js:11796993/218

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ let userinput = $(".input").val();
 
 let num = Math.random();
 console.log(num);
+
+$(".play").click(function() {
+let userinput = $(".input").val();
+ $(".computerChoice").text(num);
+});
+
+// .333>num(rock)<.6666(paper)

3d3684727c70465ede6370eb350ff553d3eeb804
Glitch (breezy-spot-color) <none>
2021-12-08 23:35:43 +0000

    🍄🎗 Checkpoint ./js/script.js:11796993/1090

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-$(".play").click(function() {
-let userinput = $(".input").val();
- $(".userChoice").text(userinput);
+// $(".choices").hide();
+
+
+$(".play").click(function(){
+  $(".choices").show();
+  
+  let userInput= $(".input").val();
+  $(".userChoice").text(userInput);
+  
+  let computerOutput= Math.ceil(Math.random() * 3);
+  //write the numbers as rock paper scissors
+  if(computerOutput === 1) {
+    let ComputerInput = "rock;"
+    // (".computerChoice").text("rock");
+} else if(computerOutput === 2) {
+      let ComputerInput = "paper;"
+    // (".computerChoice").text("paper");
+} else if (computerOutput === 3) {
+      let ComputerInput = "sicssor;"
+  // (".computerChoice").text("scissor")
+}
+ 
+ $(".shoot").click(function () {
+   if(userInput === computerInput) {
+     $("body").css("background-color","green");
+   } else{
+          $("body").css("background-color","red");
+   }
+ })
+  
+  //text of win or lose
 });
 
-let num = Math.random();
-console.log(num);
 
-$(".play").click(function() {
-let userinput = $(".input").val();
- $(".computerChoice").text(num);
-});
 
-// .333>num(rock)<.6666(paper)
